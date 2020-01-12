3aa174cf09c9267894682bbcb7916aab6daa4a85
tboyd4 <tboyd1898@gmail.com>
2020-01-11 18:14:21 -0600

    nested ajax call for UV index is working and displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 
-// openweather api key
-var city = "";
-
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f41a04a3ef06b441d0a448b858a97ca4";
-
 
 // function that displays current weather data when search bar click
 
 function searchCity (cityArg) {
-    city = cityArg;
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f41a04a3ef06b441d0a448b858a97ca4";
-    queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f41a04a3ef06b441d0a448b858a97ca4";
+    var city = cityArg;
+    let currentLon = "";
+    let currentLat = "";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f41a04a3ef06b441d0a448b858a97ca4";
+    var queryURLFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f41a04a3ef06b441d0a448b858a97ca4";
+
+    
 
     $.ajax({
         url: queryURL,
@@ -27,6 +25,22 @@ function searchCity (cityArg) {
         let resTempFar = ((resTempKelv - 273.15) * 1.80) + 32;
         resTempFar = Math.floor(resTempFar);
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+        // nested ajax call to get uv
+
+        currentLon = response.coord.lon;
+        currentLat = response.coord.lat;
+
+        var queryURLuv = "http://api.openweathermap.org/data/2.5/uvi?appid=f41a04a3ef06b441d0a448b858a97ca4&lat=" + currentLat + "&lon=" + currentLon;
+
+        $.ajax({
+            url: queryURLuv,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            let resUV = response.value;
+            setUV(resUV);
+        })
         
         setTodayInfo(resName, resTempFar, resHumi, resWind, iconUrl);
     })
@@ -48,6 +62,12 @@ function setTodayInfo (city, temp, humi, wind, icon) {
     $("#humi").text("Humidity: " + humi);
     $("#wind").text("Wind Speed: " + wind + " MPH");
     $("#wicon").attr("src", icon);
+}
+
+// function to set UV index for current day
+
+function setUV (uv) {
+    $("#uv").text("UV Index: " + uv);
 }
 
 // event listener for the search click specifically
